08bfa3dce675177c99ebc7e30e2d38400886520f
Mustapha <mostafaaantri88@gmail.com>
2023-03-27 16:38:04 +0000

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - returns the lenght of a string
+ * @s: string to be evaluated
+ * Return: lenght of the string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
