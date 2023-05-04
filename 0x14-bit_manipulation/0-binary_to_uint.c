7e044ad7d36448745b3c9caea71191834dcdbb0f
Mustapha <mostafaaantri88@gmail.com>
2023-05-04 11:21:28 +0000

    .

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,18 +10,18 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
-	int i;
 
 	if (b == NULL)
 		return (0);
 
-	for (i = 0; b[i] != '\0'; i++)
+	while (*b != '\0')
 	{
-		if (b[i] == '0' || b[i] == '1')
-			num = num * 2 + (b[i] - '0');
-		else
+		num = num << 1;
+		if (*b != '1' && *b != '0')
 			return (0);
+		else if (*b == '1')
+			num = num | 1;
+		b++;
 	}
-
 	return (num);
 }
