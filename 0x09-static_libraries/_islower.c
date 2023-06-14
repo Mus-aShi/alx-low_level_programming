7e6c4d3b8b7e5cceb96b0cda15cdf940a007ed8a
Mustapha <mostafaaantri88@gmail.com>
2023-06-14 14:26:55 +0000

    ;

--- 0x09-static_libraries/_islower.c
+++
@@ -1,5 +0,0 @@
-#include "main.h"
-
-int _islower(int c) {
-    /* Implementation of _islower function */
-}
--- 0x09-static_libraries/_putchar.c
+++
@@ -1,5 +0,0 @@
-#include "main.h"
-
-int _putchar(char c) {
-    /* Implementation of _putchar function */
-}
