a064898d12336730e3e3bbc72a3ff7f717d953e1
Mustapha <mostafaaantri88@gmail.com>
2023-10-27 22:47:11 +0000

    last for real

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,51 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+
 /**
- * main - add positive numbers
- * @argc: int
- * @argv: array of string
- * Return: 0
+ * main - Prints the addition of positive numbers,
+ *        followed by a new line.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: If one of the numbers contains symbols that are non-digits - 1.
+ *         Otherwise - 0.
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-int i, sum = 0;
-if (argc == 1)
-{
-printf("%d\n", 0);
-}
-else
-{
-for (i = 1; i < argc; i++)
-{
-if (string_digit(argv[i]))
-{
-sum = sum + atoi(argv[i]);
-}
-else
-{
-printf("Error\n");
-return (1);
-}
-}
-printf("%d\n", sum);
-}
-return (0);
-}
-/**
- * string_digit - checks if c is digit
- * @s: pointer to string
- * Return: 1 if all string is digits else 0
- */
-int string_digit(char *s)
-{
-int i, t = 1;
-for (i = 0; s[i] != '\0'; i++)
-{
-if (!((s[i] >= 48) && (s[i] <= 57)))
-{
-t = t * 0;
-}
-}
-return (t);
+	int num, digit, sum = 0;
+
+	for (num = 1; num < argc; num++)
+	{
+		for (digit = 0; argv[num][digit]; digit++)
+		{
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[num]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
 }
